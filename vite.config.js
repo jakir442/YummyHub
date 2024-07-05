a3c598b753b37bcfcfc9a4dcef1fc1c3bcadd058
jakir442 <jakigeming2021@gmail.com>
2024-07-05 19:06:17 +0700

    YummyHub

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/yummyhub/",
   plugins: [react()],
+  build: {
+    outDir: "dist", // Pastikan output directory adalah 'dist'
+  },
 });
